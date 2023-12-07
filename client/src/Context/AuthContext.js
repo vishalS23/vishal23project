f7a0a545e40e9cb39b467baab9b5b5fcbe0a336a
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-12-07 14:23:30 +0530

    Updates

---
+++ client/src/Context/AuthContext.js
@@ -0,0 +1,36 @@
+import {createContext} from 'react'
+
+export const myContext = createContext();
+
+const Reducer = (state, action) => {
+    switch (action. type){
+        case "LOGIN":
+            return{ ...state, user: action.payload}
+        case 'LOGOUT':
+            return{ ...state, user: null}
+        default:
+            return state    
+    }
+}
+
+const initialState = { user: null}
+
+const AuthContext = ({children}) => {
+    const [state, dispatch]= userReducer(Reducer, initialState);
+
+    const Login =(data) => {
+        dispatch  ({type: "LOGIN", payload: data})
+
+    }
+    const Logout =() => {
+        dispatch ({type: "LOGOUT"})
+    }
+
+    return (
+        <myContext.Provider value={{state, Login, Logout}}>
+            {children}
+        </myContext.Provider>  
+    )
+}
+
+export default AuthContext;
