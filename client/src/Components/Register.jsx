ce4db25f746c4730d2f54ab4b74658fa7b257401
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-12-08 14:50:55 +0530

    updates

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -1,6 +1,18 @@
 function Register(){
     return (
-        <div>Register</div>
+        <div>
+            <h1>Register</h1>
+            <form>
+                <label>Name</label><br/>
+                <input type= "text"/><br/>
+                <label>Email</label><br/>
+                <input type = "email"/><br/>
+                <label>Password</label><br/>
+                <input type = "password"/><br/>
+                <input type = "submit" value = "Register"/><br/>
+            </form>
+
+        </div>
     )
 }
 
